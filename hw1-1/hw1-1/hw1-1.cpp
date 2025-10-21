54c5c1a68f2b8232b0b8f338b401631bee3a317a
wqd1206 <t114360245@ntut.org.tw>
2025-10-21 17:10:18 +0800

    [hw1]

---
+++ hw1-1/hw1-1/hw1-1.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    printf("*********      ***        *            *\n");
+    printf("*       *    *     *     ***          * *\n");
+    printf("*       *   *       *   *****        *   *\n");
+    printf("*       *   *       *     *         *     *\n");
+    printf("*       *   *       *     *        *       *\n");
+    printf("*       *   *       *     *         *     *\n");
+    printf("*       *   *       *     *          *   *\n");
+    printf("*       *    *     *      *           * *\n");
+    printf("*********      ***        *            *\n");
+    return 0;
+}
+
